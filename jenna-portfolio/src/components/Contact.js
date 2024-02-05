e08a68caf12e25f3653287c76e2bfc27a106bd63
Jenna Martin <jennamar828@gmail.com>
2024-02-05 11:19:13 -0500

    contact page updated

--- jenna-portfolio/src/components/Contact.js
+++ jenna-portfolio/src/components/Contact.js
@@ -39,21 +39,21 @@ export default function Contact() {
               </p>
             </div>
             <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-envelope-fill h-4 w-4 text-blue-500"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z" />
-                </svg>
-                <p className="text-gray-50 font-light text-sm">
-                  {userData.email}
-                </p>
-              </div>
-              <div className="social-icons flex flex-row space-x-8">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-envelope-fill h-4 w-4 text-blue-500"
+                viewBox="0 0 16 16"
+              >
+                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z" />
+              </svg>
+              <p className="text-gray-50 font-light text-sm">
+                {userData.email}
+              </p>
+            </div>
+            <div className="social-icons flex flex-row space-x-8">
               <a
                 href={userData.socialLinks.facebook}
                 className="h-10 w-10 rounded-full hover:bg-blue-500 flex items-center justify-center cursor-pointer"
@@ -124,6 +124,43 @@ export default function Contact() {
               </a>
             </div>
           </div>
+          <form className="form rounded-lg bg-white p-4 flex flex-col">
+            <label htmlFor="name" className="text-sm text-gray-600 mx-4">
+              {" "}
+              Your Name
+            </label>
+            <input
+              type="text"
+              className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+              name="name"
+            />
+            <label htmlFor="email" className="text-sm text-gray-600 mx-4 mt-4">
+              Email
+            </label>
+            <input
+              type="text"
+              className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+              name="email"
+            />
+            <label
+              htmlFor="message"
+              className="text-sm text-gray-600 mx-4 mt-4"
+            >
+              Message
+            </label>
+            <textarea
+              rows="4"
+              type="text"
+              className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+              name="message"
+            ></textarea>
+            <button
+              type="submit"
+              className="bg-blue-500 rounded-md w-1/2 mx-4 mt-8 py-2 text-gray-50 text-xs font-bold"
+            >
+              Send Message
+            </button>
+          </form>
         </div>
       </div>
     </section>
