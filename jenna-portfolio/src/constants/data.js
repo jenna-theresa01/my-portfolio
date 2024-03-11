ceb372a56ee1e53f0fead9c1f7e2b28e9e83e18d
Jenna Martin <jennamar828@gmail.com>
2024-03-11 13:32:50 -0400

    experience page started

--- jenna-portfolio/src/constants/data.js
+++ jenna-portfolio/src/constants/data.js
@@ -1,78 +1,115 @@
 const userData = {
-    githubUsername: "jenna-theresa01",
-    name: "Jenna Martin",
-    designation: "Full-Stack Software Developer",
-    email: "jennamar828@gmail.com",
-    phone: "+1 8595376994",
-    projects: [
-        {
-            title: "Mind Reader",
-            link: "https://github.com/jenna-theresa01/mind-reader-project",
-            imgUrl: "/img/mind-reader.jpeg",
-        },
-        {
-            title: "Weather App",
-            link: "https://github.com/jenna-theresa01/weather-app",
-            imgUrl: "/img/weather-app.jpeg",
-        },
-        {
-            title: "Tic-Tac-Toe",
-            link: "https://github.com/jenna-theresa01/tic-tac-toe-project",
-            imgUrl: "/img/tic-tac-toe.png",
-        },
-        {
-            title: "React Restaurant",
-            link: "https://github.com/jenna-theresa01/react-restaurant",
-            imgUrl: "/img/react-restaurant.jpeg",
-        },
-        {
-            title: "To-Do List",
-            link: "https://github.com/jenna-theresa01/to-do-list",
-            imgUrl: "/img/to-do-list.jpeg",
-        },
-        {
-            title: "Python / EverCraft",
-            link: "https://github.com/jenna-theresa01/EverCraft-Game",
-            imgUrl: "/img/evercraft.png",
-        },
-        {
-            title: "SQL Heroes",
-            link: "https://github.com/jenna-theresa01/sql-heroes",
-            imgUrl: "/img/sql-hero.jpeg",
-        },
-        {
-            title: "Backend Bistro",
-            link: "https://github.com/jenna-theresa01/backend-bistro",
-            imgUrl: "/img/backend-bistro.jpeg",
-        },
-        {
-            title: "Fast.fm",
-            link: "https://github.com/jenna-theresa01/fast-fm-project",
-            imgUrl: "/img/fast.fm.png",
-        },
-        {
-            title: "Capstone Project",
-            link: "https://catch-the-creature-frontend-3vjh.vercel.app/",
-            imgUrl: "/img/catch-the-creatures.png",
-        },
-    ], 
-    about: {
-        title: "I am a software developer that loves building projects and anything animal related",
-        description: [
-            `I have been passively learning development since I was in college, but took the dive to be a serious developer in August of 2023 through Awesome Inc. Developer Bootcamp.`,
-            `I want to continue learning code and never take for granted that my learning days are done. While in bootcamp I spent 16 weeks learning and honing my skills with HTML, CSS, React, Python, and FastAPI.`, 
-            `Now I want to spend my time working on updating the code that I worked on in the bootcamp and continue my learning in other languages and frameworks.`,
-        ],
-        currentProject: "Mind Reader",
-        currentProjectURL: "https://github.com/jenna-theresa01/mind-reader-project",
+  githubUsername: "jenna-theresa01",
+  name: "Jenna Martin",
+  designation: "Full-Stack Software Developer",
+  email: "jennamar828@gmail.com",
+  phone: "+1 8595376994",
+  projects: [
+    {
+      title: "Mind Reader",
+      link: "https://github.com/jenna-theresa01/mind-reader-project",
+      imgUrl: "/img/mind-reader.jpeg",
     },
-    socialLinks: {
-        twitter: "https://twitter.com/jennamar828",
-        instagram: "https://www.instagram.com/jennamar828/",
-        linkedin: "https://www.linkedin.com/in/jenna-t-martin",
-        github: "https://github.com/jenna-theresa01", 
-    }
-
+    {
+      title: "Weather App",
+      link: "https://github.com/jenna-theresa01/weather-app",
+      imgUrl: "/img/weather-app.jpeg",
+    },
+    {
+      title: "Tic-Tac-Toe",
+      link: "https://github.com/jenna-theresa01/tic-tac-toe-project",
+      imgUrl: "/img/tic-tac-toe.png",
+    },
+    {
+      title: "React Restaurant",
+      link: "https://github.com/jenna-theresa01/react-restaurant",
+      imgUrl: "/img/react-restaurant.jpeg",
+    },
+    {
+      title: "To-Do List",
+      link: "https://github.com/jenna-theresa01/to-do-list",
+      imgUrl: "/img/to-do-list.jpeg",
+    },
+    {
+      title: "Python / EverCraft",
+      link: "https://github.com/jenna-theresa01/EverCraft-Game",
+      imgUrl: "/img/evercraft.png",
+    },
+    {
+      title: "SQL Heroes",
+      link: "https://github.com/jenna-theresa01/sql-heroes",
+      imgUrl: "/img/sql-hero.jpeg",
+    },
+    {
+      title: "Backend Bistro",
+      link: "https://github.com/jenna-theresa01/backend-bistro",
+      imgUrl: "/img/backend-bistro.jpeg",
+    },
+    {
+      title: "Fast.fm",
+      link: "https://github.com/jenna-theresa01/fast-fm-project",
+      imgUrl: "/img/fast.fm.png",
+    },
+    {
+      title: "Capstone Project",
+      link: "https://catch-the-creature-frontend-3vjh.vercel.app/",
+      imgUrl: "/img/catch-the-creatures.png",
+    },
+  ],
+  about: {
+    title:
+      "I am a software developer that loves building projects and anything animal related",
+    description: [
+      `I have been passively learning development since I was in college, but took the dive to be a serious developer in August of 2023 through Awesome Inc. Developer Bootcamp.`,
+      `I want to continue learning code and never take for granted that my learning days are done. While in bootcamp I spent 16 weeks learning and honing my skills with HTML, CSS, React, Python, and FastAPI.`,
+      `Now I want to spend my time working on updating the code that I worked on in the bootcamp and continue my learning in other languages and frameworks.`,
+    ],
+    currentProject: "Mind Reader",
+    currentProjectURL: "https://github.com/jenna-theresa01/mind-reader-project",
+  },
+  experience: [
+    {
+      title: "Software Developer",
+      company: "Awesome Inc.",
+      year: "2023",
+      companyLink: "https://awesomeinc.org/",
+      desc: "The Software Developer Bootcamp is a 4-month, full-time training program for aspiring software developers.",
+    },
+    {
+        title: "Pet Care Assistant",
+        company: "Banfield Pet Hospital",
+        year: "2022",
+        companyLink: "https://www.banfield.com/en",
+        desc: "Utilize specialized equipment and demonstrated technical aptitude in animal technology.",
+      },
+      {
+        title: "Assistant manager",
+        company: "Alumni Hall",
+        year: "2021",
+        companyLink: "https://www.alumnihall.com/info/the-summit-at-fritz-farm",
+        desc: "Spearheaded daily operations such as opening and closing the store, making deposits and keeping track of payroll.",
+      },
+      {
+        title: "Server",
+        company: "Double Dogs",
+        year: "2019",
+        companyLink: "https://doubledogs.biz/lexington/",
+        desc: "Provided excellent customer experience to patrons while working in collaboration with a team including other servers, bartenders, kitchen and management staff.",
+      },
+      {
+        title: "Assistant Store Manager",
+        company: "Altar'd State",
+        year: "2017",
+        companyLink: "https://www.altardstate.com/",
+        desc: "Organized and executed Mission Monday Give Back events through cultivated relationships with local charities resulting in 6 charities receiving financial and volunteer support.",
+      },
+  ],
+  socialLinks: {
+    twitter: "https://twitter.com/jennamar828",
+    instagram: "https://www.instagram.com/jennamar828/",
+    linkedin: "https://www.linkedin.com/in/jenna-t-martin",
+    github: "https://github.com/jenna-theresa01",
+  },
 };
 
 export default userData;
---
+++ jenna-portfolio/src/pages/experience.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ContainerBlock from "src/components/ContainerBlock";
+import Experience from "src/components/Experience";
+
+export default function experience() {
+    return (
+        <ContainerBlock title="Experience - Jenna Martin">
+            <Experience />
+        </ContainerBlock>
+    );
+}
